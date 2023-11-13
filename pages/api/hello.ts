54ebb20d90ecc434209385c7b35ad2b18456d56c
Martin Dallos <tinypin14@gmail.com>
2023-11-13 20:26:18 +0100

    test

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -3,4 +3,4 @@ import { NextApiRequest, NextApiResponse } from 'next';
 export default (req: NextApiRequest, res: NextApiResponse) => {
     res.status(200).json({ text: 'Hello' });
   }
-  
+console.log('hello');
